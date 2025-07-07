e6fbe79a30647cec97ed63622df5fdae1d1b85de
Julia Borges <juliaborgesdefreitas@hotmail.com>
2025-07-06 21:18:05 -0300

    remove code commented and add class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+import "./fantasy.css";
 
 export const metadata: Metadata = {
   title: "Library",
@@ -23,11 +13,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
-      <body
-        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body className={`fantasy-bg`}>
+          {children}
       </body>
     </html>
   );
