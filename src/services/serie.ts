273d6916d5b3064fb819a01a10e0030999976b56
Julia Borges <juliaborgesdefreitas@hotmail.com>
2025-07-11 13:52:18 -0300

    get current book title on select serie

--- src/services/serie.ts
+++ src/services/serie.ts
@@ -9,7 +9,7 @@ type SerieUpdate = Database["public"]["Tables"]["serie"]["Update"];
 export async function serieList() {
   const { data, error } = await supabase
     .from("serie")
-    .select()
+    .select(`*, book:book!serie_current_book_id_fkey(id, title)`)
     .overrideTypes<SerieRead[]>();
 
   if (error) {
